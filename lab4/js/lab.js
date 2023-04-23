5673d827ff82840567051a61cf420fa23c144f8b
Jing Li <106184593+jiexiner@users.noreply.github.com>
2023-04-23 16:26:14 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock, Paper, and Scissors
+// Author: Jing Li (jli566@ucsc.edu)
+// Date: April 25, 2023
 
 // Constants
+
+
+// Pseudocode
+
+//1. Initialize variables for user_score and computer_score to 0.
+//2. Repeat the following steps until the game ends:
+    //a. Ask the user to choose between rock, paper, or scissors.
+    //b. Generate a random choice for the computer.
+    //c. Compare the user's and computer's choices:
+        //i. If they choose the same, it's a tie.
+        //ii. Otherwise, the winner is determined as follows:
+            //1. Rock beats scissors.
+            //2. Scissors beat paper.
+            //3. Paper beats rock.
+            //If the user wins, add 1 to user_score. Otherwise, add 1 to computer_score.
+    //d. Print the result of the round.
+//3. Print the final result of the game.
 
 // Functions
 
