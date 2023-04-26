6558eb118f1e40f1f50d313077920531eac4ca8c
Jing Li <106184593+jiexiner@users.noreply.github.com>
2023-04-26 16:59:33 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,8 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+* index.js - experimenting with basic JavaScript data types and variables
+* Author:    Jing Li
+* Created:   04.27.2023
+**/
+// define Variables
+make = "Toyota";
+model = "Camry SE";
+color = "Parisian Night Pearl";
+year = 2016;
 
-// Constants
+// calculation
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // Functions
 
